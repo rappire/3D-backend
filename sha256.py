440f57f5edf2af414e8b383cfc58a3b9a1888507
rappire <rappire@gmail.com>
2023-01-26 21:44:00 +0900

    start

--- sha256.py
+++ sha256.py
@@ -1,8 +1,9 @@
 import hashlib
+
 key = "@*&an10^"
 
-def encrypt_sha(password):  
-        password = key + password      
-        print(hashlib.sha256(password.encode()).hexdigest())
-        return hashlib.sha256(password.encode()).hexdigest()
-    
+
+def encrypt_sha(password):
+    password = key + password
+    print(hashlib.sha256(password.encode()).hexdigest())
+    return hashlib.sha256(password.encode()).hexdigest()
